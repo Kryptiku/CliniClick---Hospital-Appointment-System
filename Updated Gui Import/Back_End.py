da5c40b281417f197704edc59320cb58972f640d
Aldrich Ryan Antony <54350232+Kryptiku@users.noreply.github.com>
2023-11-21 23:17:32 +0800

    Update to input validation logic

--- Updated Gui Import/Back_End.py
+++ Updated Gui Import/Back_End.py
@@ -1,46 +1,61 @@
+import customtkinter
 import datetime
 import mysql.connector
 
 db = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'cliniclick_db')
 mycur = db.cursor()
-        
-def time_validation(time):
-
-    global timeObject,formatted_time, new_time
+   
+def appointment_validation(time, date):
+    global timeObject,formatted_time, new_time, dateObject, new_date
     new_time = str(time)
     formatted_time = new_time.upper()
     time_format = '%I:%M %p'
     
+    new_date = str(date)
+    date_format = '%Y-%m-%d'
+    
     try:
         timeObject = datetime.datetime.strptime(formatted_time, time_format)
         print(formatted_time)
+        
+        dateObject = datetime.datetime.strptime(new_date, date_format)
+        print(new_date)
 
     except ValueError:
-        print(new_time)
-        print('Error')
+        global failed_main
+        failed_main = customtkinter.CTkToplevel()
+        failed_main.attributes('-topmost', True)
+        failed_main.title('Invalid Entry')
+        failed_main.geometry()
+        my_font = customtkinter.CTkFont(family = 'bold')
         
+        error_label = customtkinter.CTkLabel(failed_main, text = 'Incorrect Input', font = my_font)
+        ok_button = customtkinter.CTkButton(failed_main, text = 'Ok', bg_color = 'grey', command = fail_destroy)
+        
+        error_label.pack()
+        ok_button.pack()
 
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def validation_fail():
-#     failed_main = tk.Toplevel(main)
-#     failed_main.title('Invalid Entry')
-#     failed_main.geometry()
+# def date_validation(date):
+#     global dateObject, new_date
+#     new_date = str(date)
+#     date_format = '%Y-%m-%d'
     
-#     error_label = tk.Label(failed_main, text = 'Incorrect Time', fg = 'red', font = 'bold', width = 20)
-#     ok_button = tk.Button(failed_main, text = 'Ok', bg = 'grey', command = fail_destroy)
-    
-#     error_label.pack()
-#     ok_button.pack()
+    # try:
+    #     dateObject = datetime.datetime.strptime(new_date, date_format)
+    #     print(new_date)
+    # except ValueError:
+    #     global failed_main
+    #     failed_main = customtkinter.CTkToplevel()
+    #     failed_main.attributes('-topmost', True)
+    #     failed_main.title('Invalid Entry')
+    #     failed_main.geometry()
+    #     my_font = customtkinter.CTkFont(family = 'bold')
+        
+    #     error_label = customtkinter.CTkLabel(failed_main, text = 'Incorrect Date', font = my_font)
+    #     ok_button = customtkinter.CTkButton(failed_main, text = 'Ok', bg_color = 'grey', command = fail_destroy)
+        
+    #     error_label.pack()
+    #     ok_button.pack()
+        
+def fail_destroy():
+    failed_main.destroy()
--- Updated Gui Import/Front_End.py
+++ Updated Gui Import/Front_End.py
@@ -3,14 +3,16 @@ import Back_End as be
 
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
-main = customtkinter.CTk()
+
 
 def appointment():
-        
+    global time, date, main
+    main = customtkinter.CTk()
+
     main.title('Appointment')
     main.geometry('525x200')
+    
     my_font = customtkinter.CTkFont(family = 'bold')
-    global time
     time = customtkinter.StringVar()
     
     register_label = customtkinter.CTkLabel(main, text = 'Appointment Registration', font = my_font)
@@ -22,19 +24,25 @@ def appointment():
     label_time = customtkinter.CTkLabel(main, text='Time: ')
     entry_time = customtkinter.CTkEntry(main, textvariable = time)
     
-    appointment_button = customtkinter.CTkButton(main, text = 'Enter', command = lambda:be.time_validation(time.get()))
-   
     label_time.pack()
     entry_time.pack()
-    appointment_button.pack()
     
     date = customtkinter.StringVar()
     label_date = customtkinter.CTkLabel(main, text='Date: ')
     entry_date = customtkinter.CTkEntry(main, textvariable = date)
+    # date_button = customtkinter.CTkButton(main, text = 'Enter Date', command = date_validation)
+    appointment_button = customtkinter.CTkButton(main, text = 'Enter', command = appointment_validation)
     
     label_date.pack()
     entry_date.pack()
-    
+    # date_button.pack()
+    appointment_button.pack()
     main.mainloop()
     
+def appointment_validation():
+    be.appointment_validation(time.get(), date.get())
+    # be.date_validation(date.get())
+# def date_validation():
+    
+       
 appointment()
