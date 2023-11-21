2643f1cb3cf0439fc14b5ababf54da32eef78c42
Aldrich Ryan Antony <54350232+Kryptiku@users.noreply.github.com>
2023-11-22 02:26:25 +0800

    Updated logic

--- Updated Gui Import/Back_End.py
+++ Updated Gui Import/Back_End.py
@@ -1,14 +1,16 @@
 import customtkinter
 import datetime
 import mysql.connector
+from subprocess import call
 
 db = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'cliniclick_db')
 mycur = db.cursor()
    
-def appointment_validation(time, date):
-    global timeObject,formatted_time, new_time, dateObject, new_date, error_time, error_date
+def appointment_validation(time, date, doctor):
+    global timeObject,formatted_time, new_time, dateObject, new_date, error_time, error_date, error_doctor
     error_time = False
     error_date = False
+    error_doctor = False
     
     new_time = str(time)
     formatted_time = new_time.upper()
@@ -30,50 +32,62 @@ def appointment_validation(time, date):
         
     except ValueError:   
         error_date = True
+        
+    sql = 'select doctor_code from doctortbl where doctor_lastname = ' + '\'' + doctor + '\''
+    mycur.execute(sql)
+    results = mycur.fetchall()  
     
-    if error_date == True and error_time == False:
+    if results:
+        error_doctor = False
+    else:
+        error_doctor = True
+    
+    print(error_doctor)
+    if error_doctor == True or error_date == True or error_time == True:
         global failed_main
         failed_main = customtkinter.CTkToplevel()
         failed_main.attributes('-topmost', True)
         failed_main.title('Invalid Entry')
-        failed_main.geometry()
+        failed_main.geometry('170x70')
         my_font = customtkinter.CTkFont(family = 'bold')
         
-        error_label = customtkinter.CTkLabel(failed_main, text = 'Incorrect Date', font = my_font)
-        ok_button = customtkinter.CTkButton(failed_main, text = 'Ok', bg_color = 'grey', command = fail_destroy)
-        
-        error_label.pack()
-        ok_button.pack()
-    
-    if error_date == False and error_time == True:
-        failed_main = customtkinter.CTkToplevel()
-        failed_main.attributes('-topmost', True)
-        failed_main.title('Invalid Entry')
-        failed_main.geometry()
-        my_font = customtkinter.CTkFont(family = 'bold')
-        
-        error_label = customtkinter.CTkLabel(failed_main, text = 'Incorrect Time', font = my_font)
-        ok_button = customtkinter.CTkButton(failed_main, text = 'Ok', bg_color = 'grey', command = fail_destroy)
-        
-        error_label.pack()
-        ok_button.pack()
-    
-    elif error_date == True and error_time == True:
-        failed_main = customtkinter.CTkToplevel()
-        failed_main.attributes('-topmost', True)
-        failed_main.title('Invalid Entry')
-        failed_main.geometry()
-        my_font = customtkinter.CTkFont(family = 'bold')
-        
-        error_label = customtkinter.CTkLabel(failed_main, text = 'Incorrect Date and Time', font = my_font)
-        ok_button = customtkinter.CTkButton(failed_main, text = 'Ok', bg_color = 'grey', command = fail_destroy)
+        error_label = customtkinter.CTkLabel(failed_main, text = 'Incorrect Input', font = my_font)
+        ok_button = customtkinter.CTkButton(failed_main, text = 'Ok', command = fail_destroy)
         
         error_label.pack()
         ok_button.pack()
     
     else:
-        print(error_time)
-        print(error_date)
+        success()
+        mycur.execute('select apt_req_code from appointmentstbl')
+        mycur.fetchall()
+        conv_rowcount = str(mycur.rowcount + 1)
+        value = '00000000' 
+        conv_rowcount = str(conv_rowcount)
+        temp = len(conv_rowcount)
+        modified_value = value[:-temp]
+        db.commit()
+        sql = 'insert into appointmentstbl values (%s, %s,%s,%s,%s)'
+        t = ('AR' + modified_value + conv_rowcount, 'PA00000001', 'DO00000001', new_date, formatted_time)
+        mycur.execute(sql, t)
+        db.commit()
+
+def success():
+    global success_popup
+    success_popup = customtkinter.CTkToplevel()
+    success_popup.attributes('-topmost', True)
+    success_popup.title('Success Congrats Bitch')
+    success_popup.geometry()
+    my_font = customtkinter.CTkFont(family = 'bold')
+    
+    success_label = customtkinter.CTkLabel(success_popup, text = 'Congrats Fucker', font = my_font)
+    ok_button = customtkinter.CTkButton(success_popup, text = 'Ok', command = success_destroy)
+    
+    success_label.pack()
+    ok_button.pack()
         
 def fail_destroy():
     failed_main.destroy()
+    
+def success_destroy():
+    success_popup.destroy()
--- Updated Gui Import/Front_End.py
+++ Updated Gui Import/Front_End.py
@@ -6,38 +6,43 @@ customtkinter.set_default_color_theme('blue')
 
 
 def appointment():
-    global time, date, main
+    global time, date, doctor, main
     main = customtkinter.CTk()
 
     main.title('Appointment')
-    main.geometry('525x200')
+    main.geometry('525x300')
     
     my_font = customtkinter.CTkFont(family = 'bold')
     time = customtkinter.StringVar()
+    doctor = customtkinter.StringVar()
     
     register_label = customtkinter.CTkLabel(main, text = 'Appointment Registration', font = my_font)
-    register_label.pack()
-    
     patient_label = customtkinter.CTkLabel(main, text = 'Patient', font = my_font)
+    
+    register_label.pack()
     patient_label.pack()
-      
+    
+    doctor_label = customtkinter.CTkLabel(main, text = 'Doctor Name: ', font = my_font)
+    doctor_entry = customtkinter.CTkEntry(main, textvariable = doctor)
+    
     label_time = customtkinter.CTkLabel(main, text='Time: ')
     entry_time = customtkinter.CTkEntry(main, textvariable = time)
-    
-    label_time.pack()
-    entry_time.pack()
     
     date = customtkinter.StringVar()
     label_date = customtkinter.CTkLabel(main, text='Date: ')
     entry_date = customtkinter.CTkEntry(main, textvariable = date)
-    appointment_button = customtkinter.CTkButton(main, text = 'Enter', command = appointment_validation)
+    enter_button = customtkinter.CTkButton(main, text = 'Enter', command = appointment_validation)
     
+    doctor_label.pack()
+    doctor_entry.pack()
+    label_time.pack()
+    entry_time.pack()
     label_date.pack()
     entry_date.pack()
-    appointment_button.pack()
+    enter_button.pack(pady = 20)
     main.mainloop()
     
 def appointment_validation():
-    be.appointment_validation(time.get(), date.get())
+    be.appointment_validation(time.get(), date.get(), doctor.get())
            
 appointment()
