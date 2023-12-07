d89cfe69de92b2704f4ae2669b86a23b5ea26367
Trevor Marcos <markwilhelmtrevormarcos020304@gmail.com>
2023-12-08 03:15:37 +0800

    updated first window and login UI

--- Login/Patient_Login_FE.py
+++ Login/Patient_Login_FE.py
@@ -1,6 +1,7 @@
 import customtkinter as tk
-from tkinter import ttk
+from tkinter import *
 import Patient_Login_BE as pbe
+from PIL import ImageTk,Image
 
 tk.set_appearance_mode('System')
 tk.set_default_color_theme('blue')
@@ -16,19 +17,41 @@ def patient_main_screen():
     for child in main_screen.winfo_children():
         child.destroy()
     
-    main_screen.geometry('525x200')
-    # main_screen.attributes('-topmost', True)
+    main_screen.geometry('1280x720')
+    img1 = ImageTk.PhotoImage(Image.open("assets/pattern.jpg"))
+    l1 = tk.CTkLabel(main_screen, image=img1)
+    l1.pack()
+
+    frame = tk.CTkFrame(master=l1, width=320, height=360)
+    frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    l2 = tk.CTkLabel(master=frame, text="Hello!", font=('Century Gothic', 30))
+    l2.place(x=122, y=150)
+
+    l3 = tk.CTkLabel(master=frame, text="Welcome to the\n Cliniclick Log-In Portal", font=('Century Gothic', 10))
+    l3.place(x=110, y=190)
+
+    logo_image = Image.open("assets/logo.png")
+    logo_image = logo_image.convert("RGBA")
+
+    logo_image = logo_image.resize((350, 145))
+
+    logo = ImageTk.PhotoImage(logo_image)
+
+    logo_label = tk.CTkLabel(master=frame, image=logo, text="")
+    logo_label.image = logo
+    logo_label.place(x=23, y=23)
+
+    login_button = tk.CTkButton(master=frame, text='Log-In', command=patient_login)
+    login_button.place(x=95, y=300)
+    register_button = tk.CTkButton(master=frame, text='Register', command=patient_registration)
+    register_button.place(x=95, y=265)
     
-    welcome_label = tk.CTkLabel(main_screen, text = 'Welcom to the Cliniclick Log-In Portal')
-    login_button = tk.CTkButton(main_screen, text = 'Log-In', command = patient_login)
-    register_button = tk.CTkButton(main_screen, text = 'Register', command = patient_registration)
-    footer_label = tk.CTkLabel(main_screen, text = 'Developed By Cliniclick™')
-    
-    welcome_label.pack()
-    login_button.pack(pady = 15)
-    register_button.pack()
-    footer_label.pack(side = 'bottom', pady = 5)
-    
+    # Create a transparent label by using a Label  widget and setting its background to be transparent
+    # main_screen.wm_attributes('-transparentcolor', '#ab23ff')
+    # footer_label = tk.CTkLabel(master=main_screen, text='Developed By Cliniclick™', font=('Century Gothic', 8), fg_color="#ab23ff")
+    # footer_label.place(x=115, y=325)
+
     main_screen.mainloop()
 
 def patient_login():
@@ -39,26 +62,37 @@ def patient_login():
     username = tk.StringVar()
     password = tk.StringVar()
     
+    img1 = ImageTk.PhotoImage(Image.open("assets/pattern.jpg"))
+    l1 = tk.CTkLabel(main_screen, image=img1, text="")
+    l1.pack()
+
     main_screen.title('Log-In Portal')
-    main_screen.geometry('525x250')
-    login_label = tk.CTkLabel(main_screen, text = 'Log-In Portal')
-    username_label = tk.CTkLabel(main_screen, text = 'Username: ')
-    username_entry = tk.CTkEntry(main_screen, textvariable = username)
+    main_screen.geometry('1280x720')
+
+    frame = tk.CTkFrame(master=l1, width=320, height=360)
+    frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    l2 = tk.CTkLabel(master=frame, text="Log-In Interface", font=('Century Gothic', 30))
+    l2.place(x=45, y=50)
+
+    l3 = tk.CTkLabel(master=frame, text="Log-in the required credentials below:", font=('Century Gothic', 10))
+    l3.place(x=72, y=90)
+
+    username_label = tk.CTkLabel(master=frame,text = 'Username: ',font=('Century Gothic', 10))
+    username_label.place(x=95, y=130)
+    username_entry = tk.CTkEntry(master=frame, textvariable = username)
+    username_entry.place(x=95, y=155)
     
-    password_label = tk.CTkLabel(main_screen, text = 'Password: ')
-    password_entry = tk.CTkEntry(main_screen, textvariable = password, show = '*')
-    
-    login_button = tk.CTkButton(main_screen, text = 'Log-In',command = patient_login_verify)
-    back_button = tk.CTkButton(main_screen, text = 'Back', command = patient_main_screen)
-    
-    login_label.pack(pady = 10)
-    username_label.pack()
-    username_entry.pack()
-    password_label.pack()
-    password_entry.pack()
-    login_button.pack(pady = 10)
-    back_button.pack()
-    
+    password_label = tk.CTkLabel(master=frame, text = 'Password: ',font=('Century Gothic', 10))
+    password_label.place(x=95, y=190)
+    password_entry = tk.CTkEntry(master=frame, textvariable = password, show = '*')
+    password_entry.place(x=95, y=215)
+
+    login_button = tk.CTkButton(master=frame, text = 'Log-In',command = patient_login_verify)
+    login_button.place(x=95, y=265)
+    back_button = tk.CTkButton(master=frame, text = 'Back', command = patient_main_screen)
+    back_button.place(x=95, y=300)
+
 def patient_registration():
     global lastname
     global firstname
@@ -76,7 +110,34 @@ def patient_registration():
     registration_main = tk.CTkToplevel(main_screen)
     # registration_main.attributes('-topmost', True)
     registration_main.title('Registration Portal')
-    registration_main.geometry()
+    # Get the screen width and height
+    screen_width = registration_main.winfo_screenwidth()
+    screen_height = registration_main.winfo_screenheight()
+    
+    # Calculate the center coordinates
+    x = int((screen_width - 500) / 2)  # Width of the window is set to 500
+    y = int((screen_height - 720) / 2)  # Height of the window is set to 720
+    
+    registration_main.geometry(f"500x720+{x}+{y}")
+
+    # Load the image
+    img = Image.open("assets/pattern2.jpg")
+    background_image = ImageTk.PhotoImage(img)
+
+    # Create a label to hold the image
+    background_label = tk.CTkLabel(registration_main, image=background_image)
+    background_label.image = background_image
+    background_label.place(x=0, y=0)
+
+    # Configure rows and columns to center the widgets
+    registration_main.grid_rowconfigure(0, weight=1)
+    registration_main.grid_rowconfigure(12, weight=1)
+    registration_main.grid_columnconfigure(0, weight=1)
+    registration_main.grid_columnconfigure(3, weight=1)
+    
+    # Create an empty label for spacing in row 5
+    empty_label = tk.Label(registration_main, text="")
+    empty_label.grid(row=5, column=0)
     
     lastname = tk.StringVar()
     firstname = tk.StringVar()
@@ -89,7 +150,7 @@ def patient_registration():
     password = tk.StringVar()   
     
     title_label = tk.CTkLabel(registration_main, text = 'Register your account', width = 35)
-    title_label.grid(row = 0, columnspan = 2, pady = 10, sticky = 'n')
+    title_label.grid(row=6, column=1, columnspan=2, pady=(40, 20))
 
     label_lastname = tk.CTkLabel(registration_main, text = 'Last Name:')
     entry_lastname = tk.CTkEntry(registration_main, textvariable = lastname)
