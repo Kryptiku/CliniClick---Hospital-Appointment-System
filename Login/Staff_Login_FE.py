e0a250dd8203be0322ab9b33288776e347348b62
kebinmirabel <kebinmirabel@gmail.com>
2023-12-08 11:55:18 +0800

    add abstraction

--- Login/Staff_Login_FE.py
+++ Login/Staff_Login_FE.py
@@ -5,53 +5,141 @@ import Staff_Login_BE as sbe
 
 os.system('cls')
 
-def staff_login():                                                                                                                                   # LOGIN WINDOW   
-    global staff, username_verify, password_verify, username_entry, password_entry, loginfail_label
-    staff = tk.CTk()
-    staff.title('CliniClick Staff Log-in')
-    staff.geometry('525x300')
-    my_font = ('bold', 18)
-    
-    username_verify = tk.StringVar()
-    password_verify = tk.StringVar()
+# class LoginFrontEnd:
+#     def __init__ (self):
+#         self.staff = tk.CTk()
+#         self.staff.title('CliniClick Staff Log-in')
+#         self.staff.geometry('525x300')
+#         self.my_font = ('bold', 18)
 
-    stlogin_label = tk.CTkLabel(staff, text='Staff Log-in Portal', fg_color='black', width=525)
-    mt_label = tk.CTkLabel(staff, text='')
-    loginfail_label = tk.CTkLabel(staff, text='', font = my_font)
-    username_label = tk.CTkLabel(staff, text='Username: ')
-    username_entry = tk.CTkEntry(staff, textvariable=username_verify)
-    password_label = tk.CTkLabel(staff, text='Password: ')
-    password_entry = tk.CTkEntry(staff, textvariable=password_verify, show='*')
-    login_btn = tk.CTkButton(staff, text='Log in', command=stlogin_verify)
+#     def staff_login(self):                                                                                                                                   # LOGIN WINDOW   
+#         global staff, username_verify, password_verify, username_entry, password_entry, loginfail_label
+        
+#         username_verify = tk.StringVar()
+#         password_verify = tk.StringVar()
 
-    sbe.username_verify = username_verify
-    sbe.password_verify = password_verify
-    
-    stlogin_label.pack()
-    mt_label.pack()
-    loginfail_label.pack(side='top')
-    username_label.pack()
-    username_entry.pack()
-    password_label.pack()
-    password_entry.pack()
-    login_btn.pack(pady=20)
+#         stlogin_label = tk.CTkLabel(self.staff, text='Staff Log-in Portal', fg_color='black', width=525)
+#         mt_label = tk.CTkLabel(self.staff, text='')
+#         loginfail_label = tk.CTkLabel(self.staff, text='', font = self.my_font)
+#         username_label = tk.CTkLabel(self.staff, text='Username: ')
+#         username_entry = tk.CTkEntry(self.staff, textvariable=username_verify)
+#         password_label = tk.CTkLabel(self.staff, text='Password: ')
+#         password_entry = tk.CTkEntry(self.staff, textvariable=password_verify, show='*')
+#         login_btn = tk.CTkButton(self.staff, text='Log in', command=self.stlogin_verify)
 
-    staff.mainloop()
+#         sbe.username_verify = username_verify
+#         sbe.password_verify = password_verify
+        
+#         stlogin_label.pack()
+#         mt_label.pack()
+#         loginfail_label.pack(side='top')
+#         username_label.pack()
+#         username_entry.pack()
+#         password_label.pack()
+#         password_entry.pack()
+#         login_btn.pack(pady=20)
+
+#         self.staff.mainloop()
+
+# class LoginFunctionManager(LoginFrontEnd): #self
+#     def __init__ (self, LoginFrontEnd):
+#         super().__init__()
+#         self.loginfail_label = LoginFrontEnd.loginfail_label
+#         self.username_entry = LoginFrontEnd.username_entry
+
+#     def stlogin_verify(self):
+#         sbe.stlogin_verify_test()
+#         if sbe.results:
+#             for i in sbe.results:
+#                 st_menu()
+#                 break
+#         else:
+#             stlogin_failed()
+        
+#     def stlogin_failed(self):
+#         self.loginfail_label.configure(text='Invalid Credentials. Please try again.')
+#         username_entry.delete(0, 'end'), password_entry.delete(0, 'end')
+
+
+#     def stlogin_failed(self):
+#         loginfail_label.configure(text='Invalid Credentials. Please try again.')
+#         username_entry.delete(0, 'end'), password_entry.delete(0, 'end')
+
+class LoginFrontEnd:
+    def __init__(self):
+        self.staff = tk.CTk()
+        self.staff.title('CliniClick Staff Log-in')
+        self.staff.geometry('525x300')
+        self.my_font = ('bold', 18)
+
+    def staff_login(self):  # LOGIN WINDOW
+        global username_verify, password_verify, username_entry, password_entry, loginfail_label
+        username_verify = tk.StringVar()
+        password_verify = tk.StringVar()
+
+        stlogin_label = tk.CTkLabel(self.staff, text='Staff Log-in Portal', fg_color='black', width=525)
+        mt_label = tk.CTkLabel(self.staff, text='')
+        loginfail_label = tk.CTkLabel(self.staff, text='', font=self.my_font)
+        username_label = tk.CTkLabel(self.staff, text='Username: ')
+        username_entry = tk.CTkEntry(self.staff, textvariable=username_verify)
+        password_label = tk.CTkLabel(self.staff, text='Password: ')
+        password_entry = tk.CTkEntry(self.staff, textvariable=password_verify, show='*')
+        login_btn = tk.CTkButton(self.staff, text='Log in', command=self.stlogin_verify)
+
+        sbe.username_verify = username_verify
+        sbe.password_verify = password_verify
+
+        stlogin_label.pack()
+        mt_label.pack()
+        loginfail_label.pack(side='top')
+        username_label.pack()
+        username_entry.pack()
+        password_label.pack()
+        password_entry.pack()
+        login_btn.pack(pady=20)
+
+        self.staff.mainloop()
+
+    def stlogin_verify(self):
+        sbe.stlogin_verify_test()
+        if sbe.results:
+            for i in sbe.results:
+                st_menu()
+                break
+        else:
+            self.stlogin_failed()
+
+    def stlogin_failed(self):
+        self.loginfail_label.configure(text='Invalid Credentials. Please try again.')
+        self.username_entry.delete(0, 'end')
+        self.password_entry.delete(0, 'end')
+
+
+class LoginFunctionManager(LoginFrontEnd):
+    def __init__(self, login_frontend):
+        super().__init__()
+        self.loginfail_label = login_frontend.loginfail_label
+        self.username_entry = login_frontend.username_entry
+
+    def stlogin_failed(self):
+        super().stlogin_failed()
+        # Additional functionality for login failure, if needed
+
 
 def st_menu():                                                                                                                                    # MAIN MENU WINDOW
-    staff.geometry('525x300')
-    for child in staff.winfo_children():
+    lfe.staff.geometry('525x300')
+    for child in lfe.staff.winfo_children():
         child.destroy()
 
-    staff.title('Welcome!')
+    lfe.staff.title('Welcome!')
 
     sbe.loggedin_user = str(username_verify.get())
     sbe.getname()
-    welcome_label = tk.CTkLabel(staff, text="Welcome {} ".format(sbe.staff_lastname + ", " + sbe.staff_firstname + "!"), fg_color="green")
-    mema_label = tk.CTkLabel(staff, text="")
-    aptreq_btn = tk.CTkButton(staff, text='Appointment Requests', command=stappreq_main)
-    aptacpt_btn = tk.CTkButton(staff, text='Accepted Appointments', command = staccepted_apts_main)
-    logout_btn = tk.CTkButton(staff, text='Log-Out', bg_color='grey', width=8, height=1, command=logout)
+    welcome_label = tk.CTkLabel(lfe.staff, text="Welcome {} ".format(sbe.staff_lastname + ", " + sbe.staff_firstname + "!"), fg_color="green")
+    mema_label = tk.CTkLabel(lfe.staff, text="")
+    aptreq_btn = tk.CTkButton(lfe.staff, text='Appointment Requests', command=stappreq_main)
+    aptacpt_btn = tk.CTkButton(lfe.staff, text='Accepted Appointments', command = staccepted_apts_main)
+    logout_btn = tk.CTkButton(lfe.staff, text='Log-Out', bg_color='grey', width=8, height=1, command=logout)
     
 
     welcome_label.pack()
@@ -61,23 +149,23 @@ def st_menu():                                                                  
     logout_btn.pack()
 
 def stappreq_main():                                                                                                            # APPOINTMENT REQUESTS WINDOW
-    for child in staff.winfo_children():
+    for child in lfe.staff.winfo_children():
         child.destroy()
     
     sbe.getaptreq()
-    staff.title('Appointment Requests')
-    staff.geometry('700x650')
-    mema_label = tk.CTkLabel(staff, text="")
+    lfe.staff.title('Appointment Requests')
+    lfe.staff.geometry('700x650')
+    mema_label = tk.CTkLabel(lfe.staff, text="")
     mema_label.pack()
 
     global tree
-    tree_frame = tk.CTkFrame(staff)
+    tree_frame = tk.CTkFrame(lfe.staff)
     tree_frame.pack()
     tree = ttk.Treeview(tree_frame, show="headings")
     tree["columns"] = ("req_id", "ptntln", "ptntfn", "dctrln", "dctrfn")
     
-    bg_color = staff._apply_appearance_mode(tk.ThemeManager.theme["CTkFrame"]["fg_color"])
-    text_color = staff._apply_appearance_mode(tk.ThemeManager.theme["CTkLabel"]["text_color"])
+    bg_color = lfe.staff._apply_appearance_mode(tk.ThemeManager.theme["CTkFrame"]["fg_color"])
+    text_color = lfe.staff._apply_appearance_mode(tk.ThemeManager.theme["CTkLabel"]["text_color"])
 
     style = ttk.Style()
     style.theme_use('default')
@@ -96,27 +184,27 @@ def stappreq_main():                                                            
     aptreq_current_treeview()
     tree.pack(side = "left", fill="both", expand=True)
 
-    accept_label = tk.CTkLabel(staff, text = 'Accept appointment: ')
+    accept_label = tk.CTkLabel(lfe.staff, text = 'Accept appointment: ')
     accept_label.pack(pady = 5)
     
     sbe.ardropdownobj()
     global options
-    options = tk.CTkComboBox(staff, values = sbe.ar_options, variable = "", command=changear_entries)
+    options = tk.CTkComboBox(lfe.staff, values = sbe.ar_options, variable = "", command=changear_entries)
     options.place(relx=0.5, rely=0.5, anchor = 'center')
     options.pack()
 
     global choice_label
-    choice_label = tk.CTkLabel(staff, text = "")
+    choice_label = tk.CTkLabel(lfe.staff, text = "")
     choice_label.pack()
 
     global time, date, entry_time, entry_date
     time = tk.StringVar()
-    label_time = tk.CTkLabel(staff, text='Time (HH:MM AM/PM): ')
-    entry_time = tk.CTkEntry(staff, textvariable = time)
+    label_time = tk.CTkLabel(lfe.staff, text='Time (HH:MM AM/PM): ')
+    entry_time = tk.CTkEntry(lfe.staff, textvariable = time)
 
     date = tk.StringVar()
-    label_date = tk.CTkLabel(staff, text='Date (YYYY-MM-DD): ')
-    entry_date = tk.CTkEntry(staff, textvariable = date)
+    label_date = tk.CTkLabel(lfe.staff, text='Date (YYYY-MM-DD): ')
+    entry_date = tk.CTkEntry(lfe.staff, textvariable = date)
     
     label_time.pack()
     entry_time.pack()
@@ -124,33 +212,33 @@ def stappreq_main():                                                            
     entry_date.pack()
 
     global aptaccepted_label
-    aptaccepted_label = tk.CTkLabel(staff, text = "")
+    aptaccepted_label = tk.CTkLabel(lfe.staff, text = "")
     aptaccepted_label.pack(pady = 10)
     
-    acceptapt_btn = tk.CTkButton(staff, text = "Accept Appointment", command = verify_appointment)
+    acceptapt_btn = tk.CTkButton(lfe.staff, text = "Accept Appointment", command = verify_appointment)
     acceptapt_btn.pack(pady = 10)
 
-    back_btn = tk.CTkButton(staff, text='Back', bg_color='grey', width=8, height=1, command=st_menu)
+    back_btn = tk.CTkButton(lfe.staff, text='Back', bg_color='grey', width=8, height=1, command=st_menu)
     back_btn.pack(pady = 20)
 
 def staccepted_apts_main():                                                                                                             # ACCEPTED APPOINTMENTS WINDOW
-    for child in staff.winfo_children():
+    for child in lfe.staff.winfo_children():
         child.destroy()
 
     sbe.getacceptedapts()
-    staff.title('Accepted Appointments')
-    staff.geometry('920x700')
-    mema_label = tk.CTkLabel(staff, text="")
+    lfe.staff.title('Accepted Appointments')
+    lfe.staff.geometry('920x700')
+    mema_label = tk.CTkLabel(lfe.staff, text="")
     mema_label.pack()
 
     global acctree
-    tree_frame = tk.CTkFrame(staff)
+    tree_frame = tk.CTkFrame(lfe.staff)
     tree_frame.pack()
     acctree = ttk.Treeview(tree_frame, show="headings")
     acctree["columns"] = ("req_id", "ptntln", "ptntfn", "dctrln", "dctrfn", "aptdate", "apttime")
     
-    bg_color = staff._apply_appearance_mode(tk.ThemeManager.theme["CTkFrame"]["fg_color"])
-    text_color = staff._apply_appearance_mode(tk.ThemeManager.theme["CTkLabel"]["text_color"])
+    bg_color = lfe.staff._apply_appearance_mode(tk.ThemeManager.theme["CTkFrame"]["fg_color"])
+    text_color = lfe.staff._apply_appearance_mode(tk.ThemeManager.theme["CTkLabel"]["text_color"])
 
     style = ttk.Style()
     style.theme_use('default')
@@ -170,26 +258,26 @@ def staccepted_apts_main():                                                     
     acceptedapt_current_treeview()
     acctree.pack(side = "left", fill="both", expand=True)
 
-    update_label = tk.CTkLabel(staff, text = 'Update or Delete appointment: ')
+    update_label = tk.CTkLabel(lfe.staff, text = 'Update or Delete appointment: ')
     update_label.pack(pady = 5)
 
     sbe.aadropdownobj()
     global update_options
-    update_options = tk.CTkComboBox(staff, values = sbe.aa_options, variable = "", command=changeaa_entries)
+    update_options = tk.CTkComboBox(lfe.staff, values = sbe.aa_options, variable = "", command=changeaa_entries)
     update_options.place(relx=0.5, rely=0.5, anchor = 'center')
     update_options.pack()
 
     global updchoice_label, update_time, update_date, entry_time, entry_date
-    updchoice_label = tk.CTkLabel(staff, text = "")
+    updchoice_label = tk.CTkLabel(lfe.staff, text = "")
     updchoice_label.pack()
 
     update_time = tk.StringVar()
-    label_time = tk.CTkLabel(staff, text='Time (HH:MM AM/PM): ')
-    entry_time = tk.CTkEntry(staff, textvariable = update_time)
+    label_time = tk.CTkLabel(lfe.staff, text='Time (HH:MM AM/PM): ')
+    entry_time = tk.CTkEntry(lfe.staff, textvariable = update_time)
 
     update_date = tk.StringVar()
-    label_date = tk.CTkLabel(staff, text='Date (YYYY-MM-DD): ')
-    entry_date = tk.CTkEntry(staff, textvariable = update_date)
+    label_date = tk.CTkLabel(lfe.staff, text='Date (YYYY-MM-DD): ')
+    entry_date = tk.CTkEntry(lfe.staff, textvariable = update_date)
     
     label_time.pack()
     entry_time.pack()
@@ -197,17 +285,17 @@ def staccepted_apts_main():                                                     
     entry_date.pack()
 
     global updateapt_label
-    updateapt_label = tk.CTkLabel(staff, text = "")
+    updateapt_label = tk.CTkLabel(lfe.staff, text = "")
     updateapt_label.pack(pady = 5)
 
-    update_btn = tk.CTkButton(staff, text = 'Update Appointment', command = verify_update)
-    done_btn = tk.CTkButton(staff, text = 'Appointment Done', command = verify_done)
-    delete_btn = tk.CTkButton(staff, text = "Delete Appointment", command = delete_appointment)
+    update_btn = tk.CTkButton(lfe.staff, text = 'Update Appointment', command = verify_update)
+    done_btn = tk.CTkButton(lfe.staff, text = 'Appointment Done', command = verify_done)
+    delete_btn = tk.CTkButton(lfe.staff, text = "Delete Appointment", command = delete_appointment)
     update_btn.pack(padx = 5, pady = 5)
     done_btn.pack(padx = 5, pady = 5)
     delete_btn.pack(pady = 5)
     
-    back_btn = tk.CTkButton(staff, text='Back', bg_color='grey', width=8, height=1, command=st_menu)
+    back_btn = tk.CTkButton(lfe.staff, text='Back', bg_color='grey', width=8, height=1, command=st_menu)
     back_btn.pack(pady = 20)
 
 def verify_done():
@@ -218,7 +306,7 @@ def verify_done():
 
 def appointment_done():                                                                                                                   # APPOINTMENT DONE
     global aptdone, diagnosis, meds_cb, dosage, frequency, diagnosis_entry, dosage_entry, frequency_entry, error_label
-    aptdone = tk.CTkToplevel(staff)
+    aptdone = tk.CTkToplevel(lfe.staff)
     aptdone.attributes('-top', True)
     aptdone.title("Appointment Done")
     aptdone.geometry("400x420")
@@ -255,23 +343,6 @@ def appointment_done():                                                         
     back_btn = tk.CTkButton(aptdone, text = "Back", command = aptdone.destroy)
     back_btn.pack(pady=2)
 
-def stlogin_verify():
-    sbe.stlogin_verify_test()
-    if sbe.results:
-        for i in sbe.results:
-            st_menu()
-            break
-    else:
-        stlogin_failed()
-
-def stlogin_failed():
-    loginfail_label.configure(text='Invalid Credentials. Please try again.')
-    username_entry.delete(0, 'end'), password_entry.delete(0, 'end')
-
-def logout():
-    staff.destroy()
-    staff_login()
-
 def changear_entries(choice):
     global show_acptdapt
     show_acptdapt = choice
@@ -290,7 +361,7 @@ def verify_appointment():
             acceptappointment()
 
 def declineappointment():
-    aptfail = tk.CTkToplevel(staff)
+    aptfail = tk.CTkToplevel(lfe.staff)
     aptfail.attributes('-top', True)
     aptfail.title("Error!")
     aptfail.geometry('200x200')
@@ -340,7 +411,7 @@ def verify_update():
             update_appointment()
 
 def declineupdate():
-    updfail = tk.CTkToplevel(staff)
+    updfail = tk.CTkToplevel(lfe.staff)
     updfail.attributes('-top', True)
     updfail.title("Error!")
     updfail.geometry('200x200')
@@ -379,5 +450,10 @@ def enter_done():
         aptdone.destroy()
         updateapt_label.configure(text = "Appointment Done: " + show_updtdapt)
 
+def logout():
+    lfe.staff.destroy()
+    lfe.staff_login()
+
 # Initialize the login window
-staff_login()
+lfe = LoginFrontEnd()
+lfe.staff_login()
